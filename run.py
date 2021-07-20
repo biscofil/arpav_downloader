c2f2dde555f6704bf739ff43b62705acc8f7eb40
filippo <filippo.bisconcin@gmail.com>
2021-07-20 11:26:40 +0200

    images have timestamp as filename, ignored log file

--- run.py
+++ run.py
@@ -4,27 +4,34 @@ import os
 import urllib
 
 URL = "https://www.arpa.veneto.it/bollettini/meteo/radar/index_geo.php"
-REGEX = r"imgs/mosaico/(mosaico_[a-zA-Z0-9_.]+)"
+REGEX_URL = r"imgs/mosaico/(mosaico_[a-zA-Z0-9_.]+)"
+REGEX_TIMESTAMP = r"mosaico_([0-9]+)_mpgrid_CZ_c_mbasemax_"
 
 response = requests.get(URL)
 
 print("Request has code {}".format(response.status_code))
 
 # two groups enclosed in separate ( and ) bracket
-filenames = re.findall(REGEX, response.text)
+filenames = re.findall(REGEX_URL, response.text)
 
 for filename in filenames:
-    # filename = mosaico_202107131240_mpgrid_CZ_c_mbasemax_6510858a0270815072a151850238f3_G.png
 
+    # image_url = imgs/mosaico/mosaico_202107131240_mpgrid_CZ_c_mbasemax_6510858a0270815072a151850238f3_G.png
+
+    # https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/mosaico/mosaico_202107131320_mpgrid_CZ_c_mbasemax_291966d8209f7cf3442d8aa90ec0f2_G.png
     image_url = "https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/mosaico/" + filename
-    # image_url = https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/mosaico/mosaico_202107131240_mpgrid_CZ_c_mbasemax_6510858a0270815072a151850238f3_G.png
+
+    # only extract timestampt from the timestamp
+    res = re.search(REGEX_TIMESTAMP, filename)
+    filename = res.group(1) + '.png'
 
     # Download the file if it does not exist
     if not os.path.isfile(filename):
 
-        print('Downloading: ' + filename)
+        print('Downloading ' + image_url + ' to ' + filename)
         try:
             urllib.request.urlretrieve(image_url, filename)
+            #pass
         except Exception as inst:
             print(inst)
             print('  Encountered unknown error. Continuing.')
